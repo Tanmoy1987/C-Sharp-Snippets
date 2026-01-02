918b55ca8862dc03bb11b3025ca0b77dca0af785
Tanmoy1987 <tanmoy.saha87@gmail.com>
2026-01-02 09:19:09 -0500

    Singleton pattern demo

---
+++ Singleton_Dual_Check_Pattern.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+public sealed class SingletonObj
+{
+    private static object _lockObj = new object();
+    private static SingletonObj? _singletonObj;
+    private static int _objectCounter = 0;
+    public static SingletonObj GetSingletonInstance
+    {
+        get
+        {
+
+            if (_singletonObj == null)
+            {
+                lock (_lockObj)
+                {
+                    if (_singletonObj == null)
+                    {
+                        _singletonObj = new SingletonObj();
+                    }
+                }
+            }
+            return _singletonObj;
+        }
+    }
+    public int InstanceCounter { get { return _objectCounter; } }
+
+    #region constructor
+    private SingletonObj()
+    {
+        _objectCounter++;
+    }
+    #endregion
+}
+
+class Client
+{
+    static void Main()
+    {
+        //InitializerB();
+        //InitializerA();
+        Parallel.Invoke(() => InitializerA(), () => InitializerB());
+    }
+
+    private static void InitializerA()
+    {
+        SingletonObj objA = SingletonObj.GetSingletonInstance;
+        Console.WriteLine($"IntializerA called...instance count:{objA.InstanceCounter}");
+    }
+    private static void InitializerB()
+    {
+        //Thread.Sleep(100);
+        SingletonObj objB = SingletonObj.GetSingletonInstance;
+        Console.WriteLine($"IntializerB called...instance count:{objB.InstanceCounter}");
+    }
+}
---
+++ Singleton_Lazy_Initialization_Pattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+public sealed class SingletonObj
+{
+    private static Lazy<SingletonObj> _singletonObj = new Lazy<SingletonObj>(() => new SingletonObj());
+    private static int _objectCounter = 0;
+    public static SingletonObj GetSingletonInstance
+    {
+        get
+        {
+            return _singletonObj.Value;
+        }
+    }
+    public int InstanceCounter { get { return _objectCounter; } }
+
+    #region constructor
+    private SingletonObj()
+    {
+        _objectCounter++;
+    }
+    #endregion
+}
+
+class Client
+{
+    static void Main()
+    {
+        //InitializerB();
+        //InitializerA();
+        Parallel.Invoke(() => InitializerA(), () => InitializerB());
+    }
+
+    private static void InitializerA()
+    {
+        SingletonObj objA = SingletonObj.GetSingletonInstance;
+        Console.WriteLine($"IntializerA called...instance count:{objA.InstanceCounter}");
+    }
+    private static void InitializerB()
+    {
+        //Thread.Sleep(100);
+        SingletonObj objB = SingletonObj.GetSingletonInstance;
+        Console.WriteLine($"IntializerB called...instance count:{objB.InstanceCounter}");
+    }
+}
